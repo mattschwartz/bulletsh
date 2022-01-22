8c615106b0901d741f146b9fe7907a33ccec877d
Matt <schwamat@gmail.com>
2022-01-22 16:37:17 -0600

    added frame back

--- tools/page_viewer.py
+++ tools/page_viewer.py
@@ -8,32 +8,40 @@ term = Terminal()
 
 def on_render():
     sep = '╚' + '═' * (term.width - 2) + '╝' + term.move_down + term.move_x(0)
-    
+
     view = term.move_xy(0, 0) + term.clear + \
         term.color_rgb(80, 80, 80) + term.move_up
     view += print_title(' Jan 20 ')
 
     view += term.move_x(0)
-    view += '║ [x] Do nothing all day\n'
+    view += '║ [x] Do nothing all day' + eol_bar()
     view += sep
 
     view += print_title(' Jan 21 ')
     view += term.move_x(0)
-    view += '║ [x] Come up with better name\n║ [x] Set up git repo\n║ [-] Go to sleep\n║ [ ] Finish project\n'
+    view += ('║ [x] Come up with better name' + eol_bar() +
+             '║ [x] Set up git repo' + eol_bar() +
+             '║ [-] Go to sleep' + eol_bar() +
+             '║ [ ] Finish project' + eol_bar())
     view += sep
 
     view += TERM_COLOR_PRIMARY
     view += print_title(' Jan 22 ')
     view += term.move_x(0)
-    view += '║ [ ] Keep writing python\n║ [ ] Figure out how tf to do this shit\n'
+    view += ('║ [ ] Keep writing python' + eol_bar() +
+             '║ [ ] Figure out how tf to do this shit' + eol_bar())
     view += sep
-    
+
     view += print_help()
 
     print(view, end='')
 
     with term.cbreak():
         return term.inkey()
+
+
+def eol_bar():
+    return term.move_x(term.width - 1) + '║\n'
 
 
 def print_border():
